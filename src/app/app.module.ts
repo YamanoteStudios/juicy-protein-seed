4b25a7fb3839e4f856d287dd3038cfb28af8f12e
Chris Samuel <ksamuel.chris@gmail.com>
2018-05-10 20:23:37 -0400

    added HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ const AUTHENTICATION_MODULES = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     ...AUTHENTICATION_MODULES,
     ...MATERIAL_MODULES,
     StoreModule.forRoot(reducers),
