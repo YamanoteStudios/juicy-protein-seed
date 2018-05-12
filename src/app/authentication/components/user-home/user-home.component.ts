4d77bff56d03cb8096cb5072168f06443f81a2ea
Chris Samuel <ksamuel.chris@gmail.com>
2018-05-11 21:32:03 -0400

    coding the userHomeComponent

--- src/app/authentication/components/user-home/user-home.component.ts
+++ src/app/authentication/components/user-home/user-home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromStore from 'src/app/state';
+
 
 @Component({
   selector: 'newsn-user-home',
   templateUrl: './user-home.component.html',
   styleUrls: ['./user-home.component.css']
 })
-export class UserHomeComponent implements OnInit {
+export class UserHomeComponent {
 
-  constructor() { }
+  constructor(private store: Store<fromStore.State>) { }
 
-  ngOnInit() {
-  }
+    logout(){
+      // create a dispatch  fromstore to logOut.
+      console.log(' Logout function called !!')
+    }
 
 }
--- src/app/authentication/route-authentication.module.ts
+++ src/app/authentication/route-authentication.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginViewComponent } from './components/login-view/login-view.component';
+import { UserHomeComponent } from './components/user-home/user-home.component';
 
   @NgModule({
     imports: [
       RouterModule.forRoot([
-        { path: 'login', component: LoginViewComponent }
+        { path: 'login', component: LoginViewComponent },
+        {
+          path: 'home',
+          component: UserHomeComponent,
+         // TODO: create a AuthGuardService
+        }
+        
   ]),
     ],
     exports: [
