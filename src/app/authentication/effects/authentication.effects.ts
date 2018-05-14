9bb7356c8acd4a6614cdda8032983fac680f168c
Chris Samuel <ksamuel.chris@gmail.com>
2018-05-14 15:45:32 -0400

    create service for authentication effects

---
+++ src/app/authentication/effects/authentication.effects.ts
@@ -0,0 +1,92 @@
+// Third Party Modules
+import { Injectable } from '@angular/core';
+import { Actions, Effect } from '@ngrx/effects';
+import { map, exhaustMap, catchError, tap, mergeMap } from 'rxjs/operators';
+import { of, empty } from 'rxjs';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+
+
+// Custom Code
+import {
+  CredentialsActions,
+  CredentialsActionTypes,
+  Login,
+  LoginSuccess,
+  LoginFailure,
+  Logout,
+  LogoutConfirmed,
+  LogoutComplete,
+  LogoutCancelled,
+} from '../actions/authentication.actions';
+import { AuthenticationService } from '../services/authentication.service';
+import { LogoutPromptComponent } from '../components/logout-prompt/logout-prompt.component';
+import { createNgModule } from '@angular/compiler/src/core';
+
+
+
+@Injectable()
+export class AuthenticationEffects {
+    // ngrx Effect Decorator
+    @Effect()
+    login$ = this.actions$
+    .ofType<Login>(CredentialsActionTypes.Login)
+    .pipe(
+      map(action => action.payload),
+      exhaustMap(auth =>
+        this.authService
+          .login(auth)
+          .pipe(
+            map(user => new LoginSuccess({ user })),
+            catchError(error => of(new LoginFailure(error))),
+          ),
+      ),
+    );
+
+  @Effect({ dispatch: false })
+//   loginRedirect$ = this.actions$
+//     .ofType<LoginSuccess>(CredentialsActionTypes.LoginSuccess)
+//     .pipe(tap(() => this.router.navigate(['/books'])));
+
+  @Effect()
+  logoutConfirmation$ = this.actions$
+    .ofType<Logout>(CredentialsActionTypes.Logout)
+    .pipe(
+      exhaustMap(() =>
+        this.dialogService
+          .open(LogoutPromptComponent)
+          .afterClosed()
+          .pipe(
+            map(confirmed => {
+              if (confirmed) {
+                return new LogoutConfirmed();
+              } else {
+                return new LogoutCancelled();
+              }
+            })
+          ),
+      ),
+    );
+
+  @Effect({ dispatch: false })
+  logout$ = this.actions$
+    .ofType<LogoutConfirmed>(CredentialsActionTypes.LogoutConfirmed)
+    .pipe(
+      exhaustMap(auth =>
+        this.authService
+          .logout()
+          .pipe(
+            tap(() => this.router.navigate(['/login'])),
+            map(() => new LogoutComplete()),
+            catchError(() => of(new LogoutComplete())),
+          ),
+      ),
+    );
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthenticationService,
+    private router: Router,
+    private dialogService: MatDialog,
+  ) {}
+}
